c559d58ec0c5aaa8948f0644c46a60f01ac931f2
Goltzishpt <gilzagoltz@gmail.com>
2022-11-27 13:46:03 +0200

    Update settings.py

--- ablog/settings.py
+++ ablog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = True
 
 ALLOWED_HOSTS = ['blog-for-my-friends.herokuapp.com', '127.0.0.1']
 
