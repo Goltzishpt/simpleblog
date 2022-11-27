8403a5b5102d062f9df96b71f16e1ba418a4bedd
Goltzishpt <gilzagoltz@gmail.com>
2022-11-27 14:29:48 +0200

    Update settings.py

--- ablog/settings.py
+++ ablog/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ablog.apps.BlogConfig',
     'theblog',
     'members',
     'ckeditor'
