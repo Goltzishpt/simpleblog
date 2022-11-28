277f001c944b62cd52410e09a62b08f60aa9cc9c
Goltzishpt <gilzagoltz@gmail.com>
2022-11-28 23:56:36 +0200

    Update settings.py

--- ablog/settings.py
+++ ablog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['blog-for-my-friends.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['GilzaGoltz.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
