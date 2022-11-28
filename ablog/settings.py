30daa27d3f689d5da00f978994c50e5c741a58e0
Goltzishpt <gilzagoltz@gmail.com>
2022-11-29 00:28:08 +0200

    settings

--- ablog/settings.py
+++ ablog/settings.py
@@ -26,10 +26,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['GilzaGoltz.pythonanywhere.com', '127.0.0.1']
+
+
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS.copy()
 
 
 # Application definition
